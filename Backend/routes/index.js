edcbc03e79b958573f5d5c860edefab900bfe5be
DESKTOP-MUBI1GV <sameerjoshi42@gmail.com>
2021-11-14 01:28:10 -0800

    finally

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -12,7 +12,7 @@ var kafka = require('../kafka/client');
 const {checkAuth} = require("../Utils/passport");
 const Login = require("./Login");
 const { auth } = require("../Utils/passport");
-const {checkRestroAuth} = require("../Utils/Restro_passport");
+// const {checkRestroAuth} = require("../Utils/Restro_passport");
 // const { restroauth } = require("../utils/Restro_passport");
 auth();
 // restroauth();
